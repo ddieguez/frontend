c660eda2f09c17529db43f7d7f61ac9c8bdc37ca
Adriano Zorzi <adizorzi@gmail.com>
2020-11-04 00:47:41 -0300

    Entrega do teste

---
+++ explicacao.md
@@ -0,0 +1,2 @@
+#Escolha pré-processador css.
+Foi escolhido o SCSS, pelo fato de ter sido o ultimo pré-processador que tive contado.
--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import { InputLabel } from '@material-ui/core';
+import { Select } from '@material-ui/core';
+import { MenuItem } from '@material-ui/core';
+import { FormHelperText } from '@material-ui/core';
+import { FormControl } from '@material-ui/core';
+import { Input } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import './App.css';
-
+import './App.scss';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container width="sm">
+      <InputSelect />
+      <InputText />
+    </Container>
   );
 }
 
+class InputSelect extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: 'pr'};
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <div className="tamanho">
+        <InputLabel id="label">Estado</InputLabel>
+          <Select value={this.state.value} onChange={this.handleChange}>
+            <MenuItem value="sc">Santa Catarina</MenuItem>
+            <MenuItem value="pr">Paraná</MenuItem>
+          </Select>
+      </div>
+    );
+  }
+}
+
+
+class InputText extends React.Component{
+  render() {
+      return (
+        <FormControl className="tamanho">
+          <InputLabel htmlFor="my-input">E-mail</InputLabel>
+          <Input id="my-input" aria-describedby="my-helper-text" />
+          <FormHelperText id="my-helper-text">Nós nunca iremos compartilhar seu e-mail.</FormHelperText>
+        </FormControl>
+      )
+  }
+}
 export default App;
